8d6b73bc4fa9daa225a9bb8fee7078fec8cee09e
Elijah Gavett <egavett35@gmail.com>
2017-10-05 17:43:34 -0700

    Project Creation; built Core Data schema

---
+++ 5eSpellbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  5eSpellbook
+//
+//  Created by Elijah Gavett on 10/5/17.
+//  Copyright © 2017 Elijah Gavett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
